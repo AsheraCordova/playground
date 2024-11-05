ff7a2d1b01978d2aabf3149b7360e6762c9cc75e
Ashera Cordova <ashera.cordova@gmail.com>
2024-11-05 08:37:49 +0530

    Added examples of nav view and tablayout.

--- www/js/index.js
+++ www/js/index.js
@@ -1400,11 +1400,13 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   blurredView: function() { return /* binding */ blurredView; },
 /* harmony export */   drawer_layout: function() { return /* binding */ drawer_layout; },
-/* harmony export */   drawerlayoutstart: function() { return /* binding */ drawerlayoutstart; }
+/* harmony export */   drawerlayoutstart: function() { return /* binding */ drawerlayoutstart; },
+/* harmony export */   navigation_view: function() { return /* binding */ navigation_view; }
 /* harmony export */ });
 var drawer_layout = '@+id/drawer_layout';
 var blurredView = '@+id/blurredView';
 var drawerlayoutstart = '@+id/drawerlayoutstart';
+var navigation_view = '@+id/navigation_view';
 
 /***/ }),
 
@@ -1562,9 +1564,7 @@ var ButtonImpl = /** @class */function (_super) {
     this.width = undefined;
     this.text = undefined;
     this.textSize = undefined;
-    this.drawableLeft = undefined;
     this.drawableStart = undefined;
-    this.drawableRight = undefined;
     this.drawableEnd = undefined;
     this.drawableTop = undefined;
     this.drawableBottom = undefined;
@@ -1588,6 +1588,7 @@ var ButtonImpl = /** @class */function (_super) {
     this.paddingHorizontal = undefined;
     this.paddingVertical = undefined;
     this.textFormat = undefined;
+    this.drawableIconSize = undefined;
     return this.thisPointer;
   };
   ButtonImpl.prototype.tryGetDrawablePadding = function () {
@@ -2558,17 +2559,6 @@ var ButtonImpl = /** @class */function (_super) {
     this.textSize.setOrderSet(this.orderSet);
     return this.thisPointer;
   };
-  ButtonImpl.prototype.setDrawableLeft = function (value) {
-    this.resetIfRequired();
-    if (this.drawableLeft == null || this.drawableLeft == undefined) {
-      this.drawableLeft = new _widget_CommandAttr__WEBPACK_IMPORTED_MODULE_0__["default"]();
-    }
-    this.drawableLeft.setSetter(true);
-    this.drawableLeft.setValue(value);
-    this.orderSet++;
-    this.drawableLeft.setOrderSet(this.orderSet);
-    return this.thisPointer;
-  };
   ButtonImpl.prototype.setDrawableStart = function (value) {
     this.resetIfRequired();
     if (this.drawableStart == null || this.drawableStart == undefined) {
@@ -2578,17 +2568,6 @@ var ButtonImpl = /** @class */function (_super) {
     this.drawableStart.setValue(value);
     this.orderSet++;
     this.drawableStart.setOrderSet(this.orderSet);
-    return this.thisPointer;
-  };
-  ButtonImpl.prototype.setDrawableRight = function (value) {
-    this.resetIfRequired();
-    if (this.drawableRight == null || this.drawableRight == undefined) {
-      this.drawableRight = new _widget_CommandAttr__WEBPACK_IMPORTED_MODULE_0__["default"]();
-    }
-    this.drawableRight.setSetter(true);
-    this.drawableRight.setValue(value);
-    this.orderSet++;
-    this.drawableRight.setOrderSet(this.orderSet);
     return this.thisPointer;
   };
   ButtonImpl.prototype.setDrawableEnd = function (value) {
@@ -2945,6 +2924,17 @@ var ButtonImpl = /** @class */function (_super) {
     this.textFormat.setOrderSet(this.orderSet);
     return this.thisPointer;
   };
+  ButtonImpl.prototype.setDrawableIconSize = function (value) {
+    this.resetIfRequired();
+    if (this.drawableIconSize == null || this.drawableIconSize == undefined) {
+      this.drawableIconSize = new _widget_CommandAttr__WEBPACK_IMPORTED_MODULE_0__["default"]();
+    }
+    this.drawableIconSize.setSetter(true);
+    this.drawableIconSize.setValue(value);
+    this.orderSet++;
+    this.drawableIconSize.setOrderSet(this.orderSet);
+    return this.thisPointer;
+  };
   __decorate([(0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_5__.Type)(function () {
     return _widget_CommandAttr__WEBPACK_IMPORTED_MODULE_0__["default"];
   })), (0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_6__.Expose)({
@@ -3138,18 +3128,8 @@ var ButtonImpl = /** @class */function (_super) {
   __decorate([(0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_5__.Type)(function () {
     return _widget_CommandAttr__WEBPACK_IMPORTED_MODULE_0__["default"];
   })), (0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_6__.Expose)({
-    name: "drawableLeft"
-  })), __metadata("design:type", Object)], ButtonImpl.prototype, "drawableLeft", void 0);
-  __decorate([(0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_5__.Type)(function () {
-    return _widget_CommandAttr__WEBPACK_IMPORTED_MODULE_0__["default"];
-  })), (0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_6__.Expose)({
     name: "drawableStart"
   })), __metadata("design:type", Object)], ButtonImpl.prototype, "drawableStart", void 0);
-  __decorate([(0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_5__.Type)(function () {
-    return _widget_CommandAttr__WEBPACK_IMPORTED_MODULE_0__["default"];
-  })), (0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_6__.Expose)({
-    name: "drawableRight"
-  })), __metadata("design:type", Object)], ButtonImpl.prototype, "drawableRight", void 0);
   __decorate([(0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_5__.Type)(function () {
     return _widget_CommandAttr__WEBPACK_IMPORTED_MODULE_0__["default"];
   })), (0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_6__.Expose)({
@@ -3265,6 +3245,11 @@ var ButtonImpl = /** @class */function (_super) {
   })), (0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_6__.Expose)({
     name: "textFormat"
   })), __metadata("design:type", Object)], ButtonImpl.prototype, "textFormat", void 0);
+  __decorate([(0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_5__.Type)(function () {
+    return _widget_CommandAttr__WEBPACK_IMPORTED_MODULE_0__["default"];
+  })), (0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_6__.Expose)({
+    name: "drawableIconSize"
+  })), __metadata("design:type", Object)], ButtonImpl.prototype, "drawableIconSize", void 0);
   __decorate([(0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_7__.Exclude)()), __metadata("design:type", Object)], ButtonImpl.prototype, "thisPointer", void 0);
   return ButtonImpl;
 }(_ViewImpl__WEBPACK_IMPORTED_MODULE_4__.ViewImpl);
@@ -4730,9 +4715,7 @@ var EditTextImpl = /** @class */function (_super) {
     this.hintTextFormat = undefined;
     this.text = undefined;
     this.textSize = undefined;
-    this.drawableLeft = undefined;
     this.drawableStart = undefined;
-    this.drawableRight = undefined;
     this.drawableEnd = undefined;
     this.drawableTop = undefined;
     this.drawableBottom = undefined;
@@ -4756,6 +4739,7 @@ var EditTextImpl = /** @class */function (_super) {
     this.paddingTop = undefined;
     this.paddingHorizontal = undefined;
     this.paddingVertical = undefined;
+    this.drawableIconSize = undefined;
     return this.thisPointer;
   };
   EditTextImpl.prototype.tryGetCursorVisible = function () {
@@ -6005,17 +5989,6 @@ var EditTextImpl = /** @class */function (_super) {
     this.textSize.setOrderSet(this.orderSet);
     return this.thisPointer;
   };
-  EditTextImpl.prototype.setDrawableLeft = function (value) {
-    this.resetIfRequired();
-    if (this.drawableLeft == null || this.drawableLeft == undefined) {
-      this.drawableLeft = new _widget_CommandAttr__WEBPACK_IMPORTED_MODULE_0__["default"]();
-    }
-    this.drawableLeft.setSetter(true);
-    this.drawableLeft.setValue(value);
-    this.orderSet++;
-    this.drawableLeft.setOrderSet(this.orderSet);
-    return this.thisPointer;
-  };
   EditTextImpl.prototype.setDrawableStart = function (value) {
     this.resetIfRequired();
     if (this.drawableStart == null || this.drawableStart == undefined) {
@@ -6025,17 +5998,6 @@ var EditTextImpl = /** @class */function (_super) {
     this.drawableStart.setValue(value);
     this.orderSet++;
     this.drawableStart.setOrderSet(this.orderSet);
-    return this.thisPointer;
-  };
-  EditTextImpl.prototype.setDrawableRight = function (value) {
-    this.resetIfRequired();
-    if (this.drawableRight == null || this.drawableRight == undefined) {
-      this.drawableRight = new _widget_CommandAttr__WEBPACK_IMPORTED_MODULE_0__["default"]();
-    }
-    this.drawableRight.setSetter(true);
-    this.drawableRight.setValue(value);
-    this.orderSet++;
-    this.drawableRight.setOrderSet(this.orderSet);
     return this.thisPointer;
   };
   EditTextImpl.prototype.setDrawableEnd = function (value) {
@@ -6392,6 +6354,17 @@ var EditTextImpl = /** @class */function (_super) {
     this.paddingVertical.setOrderSet(this.orderSet);
     return this.thisPointer;
   };
+  EditTextImpl.prototype.setDrawableIconSize = function (value) {
+    this.resetIfRequired();
+    if (this.drawableIconSize == null || this.drawableIconSize == undefined) {
+      this.drawableIconSize = new _widget_CommandAttr__WEBPACK_IMPORTED_MODULE_0__["default"]();
+    }
+    this.drawableIconSize.setSetter(true);
+    this.drawableIconSize.setValue(value);
+    this.orderSet++;
+    this.drawableIconSize.setOrderSet(this.orderSet);
+    return this.thisPointer;
+  };
   __decorate([(0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_5__.Type)(function () {
     return _widget_CommandAttr__WEBPACK_IMPORTED_MODULE_0__["default"];
   })), (0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_6__.Expose)({
@@ -6690,18 +6663,8 @@ var EditTextImpl = /** @class */function (_super) {
   __decorate([(0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_5__.Type)(function () {
     return _widget_CommandAttr__WEBPACK_IMPORTED_MODULE_0__["default"];
   })), (0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_6__.Expose)({
-    name: "drawableLeft"
-  })), __metadata("design:type", Object)], EditTextImpl.prototype, "drawableLeft", void 0);
-  __decorate([(0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_5__.Type)(function () {
-    return _widget_CommandAttr__WEBPACK_IMPORTED_MODULE_0__["default"];
-  })), (0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_6__.Expose)({
     name: "drawableStart"
   })), __metadata("design:type", Object)], EditTextImpl.prototype, "drawableStart", void 0);
-  __decorate([(0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_5__.Type)(function () {
-    return _widget_CommandAttr__WEBPACK_IMPORTED_MODULE_0__["default"];
-  })), (0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_6__.Expose)({
-    name: "drawableRight"
-  })), __metadata("design:type", Object)], EditTextImpl.prototype, "drawableRight", void 0);
   __decorate([(0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_5__.Type)(function () {
     return _widget_CommandAttr__WEBPACK_IMPORTED_MODULE_0__["default"];
   })), (0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_6__.Expose)({
@@ -6817,6 +6780,11 @@ var EditTextImpl = /** @class */function (_super) {
   })), (0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_6__.Expose)({
     name: "paddingVertical"
   })), __metadata("design:type", Object)], EditTextImpl.prototype, "paddingVertical", void 0);
+  __decorate([(0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_5__.Type)(function () {
+    return _widget_CommandAttr__WEBPACK_IMPORTED_MODULE_0__["default"];
+  })), (0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_6__.Expose)({
+    name: "drawableIconSize"
+  })), __metadata("design:type", Object)], EditTextImpl.prototype, "drawableIconSize", void 0);
   __decorate([(0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_7__.Exclude)()), __metadata("design:type", Object)], EditTextImpl.prototype, "thisPointer", void 0);
   return EditTextImpl;
 }(_ViewImpl__WEBPACK_IMPORTED_MODULE_4__.ViewImpl);
@@ -9001,9 +8969,7 @@ var TextViewImpl = /** @class */function (_super) {
     this.onTextChange = undefined;
     this.text = undefined;
     this.textSize = undefined;
-    this.drawableLeft = undefined;
     this.drawableStart = undefined;
-    this.drawableRight = undefined;
     this.drawableEnd = undefined;
     this.drawableTop = undefined;
     this.drawableBottom = undefined;
@@ -9035,6 +9001,7 @@ var TextViewImpl = /** @class */function (_super) {
     this.paddingVertical = undefined;
     this.autoSizePresetSizes = undefined;
     this.textFormat = undefined;
+    this.drawableIconSize = undefined;
     return this.thisPointer;
   };
   TextViewImpl.prototype.tryGetAutoLink = function () {
@@ -10190,17 +10157,6 @@ var TextViewImpl = /** @class */function (_super) {
     this.textSize.setOrderSet(this.orderSet);
     return this.thisPointer;
   };
-  TextViewImpl.prototype.setDrawableLeft = function (value) {
-    this.resetIfRequired();
-    if (this.drawableLeft == null || this.drawableLeft == undefined) {
-      this.drawableLeft = new _widget_CommandAttr__WEBPACK_IMPORTED_MODULE_0__["default"]();
-    }
-    this.drawableLeft.setSetter(true);
-    this.drawableLeft.setValue(value);
-    this.orderSet++;
-    this.drawableLeft.setOrderSet(this.orderSet);
-    return this.thisPointer;
-  };
   TextViewImpl.prototype.setDrawableStart = function (value) {
     this.resetIfRequired();
     if (this.drawableStart == null || this.drawableStart == undefined) {
@@ -10210,17 +10166,6 @@ var TextViewImpl = /** @class */function (_super) {
     this.drawableStart.setValue(value);
     this.orderSet++;
     this.drawableStart.setOrderSet(this.orderSet);
-    return this.thisPointer;
-  };
-  TextViewImpl.prototype.setDrawableRight = function (value) {
-    this.resetIfRequired();
-    if (this.drawableRight == null || this.drawableRight == undefined) {
-      this.drawableRight = new _widget_CommandAttr__WEBPACK_IMPORTED_MODULE_0__["default"]();
-    }
-    this.drawableRight.setSetter(true);
-    this.drawableRight.setValue(value);
-    this.orderSet++;
-    this.drawableRight.setOrderSet(this.orderSet);
     return this.thisPointer;
   };
   TextViewImpl.prototype.setDrawableEnd = function (value) {
@@ -10729,6 +10674,17 @@ var TextViewImpl = /** @class */function (_super) {
     this.textFormat.setOrderSet(this.orderSet);
     return this.thisPointer;
   };
+  TextViewImpl.prototype.setDrawableIconSize = function (value) {
+    this.resetIfRequired();
+    if (this.drawableIconSize == null || this.drawableIconSize == undefined) {
+      this.drawableIconSize = new _widget_CommandAttr__WEBPACK_IMPORTED_MODULE_0__["default"]();
+    }
+    this.drawableIconSize.setSetter(true);
+    this.drawableIconSize.setValue(value);
+    this.orderSet++;
+    this.drawableIconSize.setOrderSet(this.orderSet);
+    return this.thisPointer;
+  };
   __decorate([(0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_5__.Type)(function () {
     return _widget_CommandAttr__WEBPACK_IMPORTED_MODULE_0__["default"];
   })), (0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_6__.Expose)({
@@ -10967,18 +10923,8 @@ var TextViewImpl = /** @class */function (_super) {
   __decorate([(0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_5__.Type)(function () {
     return _widget_CommandAttr__WEBPACK_IMPORTED_MODULE_0__["default"];
   })), (0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_6__.Expose)({
-    name: "drawableLeft"
-  })), __metadata("design:type", Object)], TextViewImpl.prototype, "drawableLeft", void 0);
-  __decorate([(0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_5__.Type)(function () {
-    return _widget_CommandAttr__WEBPACK_IMPORTED_MODULE_0__["default"];
-  })), (0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_6__.Expose)({
     name: "drawableStart"
   })), __metadata("design:type", Object)], TextViewImpl.prototype, "drawableStart", void 0);
-  __decorate([(0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_5__.Type)(function () {
-    return _widget_CommandAttr__WEBPACK_IMPORTED_MODULE_0__["default"];
-  })), (0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_6__.Expose)({
-    name: "drawableRight"
-  })), __metadata("design:type", Object)], TextViewImpl.prototype, "drawableRight", void 0);
   __decorate([(0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_5__.Type)(function () {
     return _widget_CommandAttr__WEBPACK_IMPORTED_MODULE_0__["default"];
   })), (0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_6__.Expose)({
@@ -11134,6 +11080,11 @@ var TextViewImpl = /** @class */function (_super) {
   })), (0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_6__.Expose)({
     name: "textFormat"
   })), __metadata("design:type", Object)], TextViewImpl.prototype, "textFormat", void 0);
+  __decorate([(0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_5__.Type)(function () {
+    return _widget_CommandAttr__WEBPACK_IMPORTED_MODULE_0__["default"];
+  })), (0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_6__.Expose)({
+    name: "drawableIconSize"
+  })), __metadata("design:type", Object)], TextViewImpl.prototype, "drawableIconSize", void 0);
   __decorate([(0,ts_mixer__WEBPACK_IMPORTED_MODULE_3__.decorate)((0,class_transformer__WEBPACK_IMPORTED_MODULE_7__.Exclude)()), __metadata("design:type", Object)], TextViewImpl.prototype, "thisPointer", void 0);
   return TextViewImpl;
 }(_ViewImpl__WEBPACK_IMPORTED_MODULE_4__.ViewImpl);
@@ -31268,7 +31219,7 @@ var mix = function mix() {
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be in strict mode.
+// This entry needs to be wrapped in an IIFE because it needs to be in strict mode.
 !function() {
 "use strict";
 /*!********************!*\
